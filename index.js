67be010e51db882dfb25d3d11215c43e81025ced
Taylor Tellman <taylortellman@gmail.com>
2018-05-10 15:52:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log('hello');
 }
 function sayHiToGrandma(string) {
-   if (toLowerCase');
+   if (string.toLowerCase( ===);
   return "I can\'t hear you!";
 }
 
